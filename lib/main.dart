10719bcbfd09fdff9bfd9391399fb272f88b94e9
Gabriel Castro <gabrielcastromail@gmail.com>
2022-11-08 21:33:26 -0300

    feat: results page

--- lib/main.dart
+++ lib/main.dart
@@ -10,46 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-<<<<<<< HEAD
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:[  ],
-        ),
-=======
-        primarySwatch: Colors.lightBlue,
->>>>>>> 668b27ff6732735b48d23c7b914a46810b9ea7d3
-      ),
-      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,21 +1,118 @@
+import 'dart:ui';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final _index = 1;
+  final _itemsNavigationBar = <Widget>[
+    const Icon(
+      Icons.home,
+      size: 30,
+      color: Colors.white,
+    ),
+    const Icon(
+      Icons.games,
+      size: 30,
+      color: Colors.white,
+    ),
+    const Icon(
+      Icons.lightbulb_sharp,
+      size: 30,
+      color: Colors.white,
+    ),
+    const Icon(
+      Icons.accessibility_new_outlined,
+      size: 30,
+      color: Colors.white,
+    ),
+  ];
+
+  MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlue.shade800,
-      body: Column(
-        children: [
-          Container(
-            height: 300,
-            decoration: const BoxDecoration(
-              color: Colors.white,
+      extendBody: true,
+      backgroundColor: const Color(0xFFF2F2F2),
+      appBar: AppBar(
+        title: const Text(
+          'Resultados',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.black54,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color(0xFFFFFFFF),
+        elevation: 0.0,
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        items: _itemsNavigationBar,
+        height: 70,
+        index: _index,
+        backgroundColor: Colors.transparent,
+        buttonBackgroundColor: const Color.fromARGB(255, 73, 182, 115),
+        color: const Color.fromARGB(255, 77, 122, 206),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: SafeArea(
+            child: Expanded(
+          child: ListView.builder(
+            itemCount: 6,
+            itemBuilder: (context, index) => Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              padding: const EdgeInsets.all(20),
+              height: 150,
+              decoration: const BoxDecoration(
+                color: Color(0xFFFFFFFF),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '0 - 6',
+                    style: TextStyle(color: Colors.red[400], fontSize: 17),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'ss',
+                        style: TextStyle(color: Colors.black54, fontSize: 17),
+                      ),
+                      Icon(Icons.account_balance),
+                      Text(
+                        'ss',
+                        style: TextStyle(color: Colors.black54, fontSize: 17),
+                      ),
+                      Icon(Icons.account_balance),
+                      Text(
+                        'ss',
+                        style: TextStyle(color: Colors.black54, fontSize: 17),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'sas',
+                    style: TextStyle(color: Colors.black54, fontSize: 17),
+                  )
+                ],
+              ),
             ),
           ),
-        ],
+        )),
       ),
     );
   }
